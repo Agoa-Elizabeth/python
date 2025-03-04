06babe828da246bc6b5d767f8ebb3c6fb6d615ec
Agoa Elizabeth <elizabethagoa@gmail.com>
2025-03-04 23:05:48 +0300

    all changes

---
+++ Desktop/ELIZ/takeaway.py
@@ -0,0 +1,77 @@
+#declaring varaibles
+num1 = 10
+num2 = 20
+print(num1)
+print(num1 + num2)
+
+#declare list
+numbers = [10, 20, 30, 40, 50]
+
+#dict
+fruits = {"apple", "mangoes", "grapes", "berries"}
+
+#tuple its ordered,and doesnt allow to add or even remove any value
+num = (5, 10, 15, 20)
+print(12 not in numbers)
+print(15 is not numbers)
+print(20 is 20)
+
+#comparision
+num1 > num2
+num1< num2
+num1 >= num2
+num1 <= num2
+#! means is not
+num2 ! num2
+#== means equals
+num2 == num1
+
+#logical operators
+print((True)and(True))
+print((True)and(False))
+print((True)or(True))
+print((True)or(False))
+
+#nested list
+items= [100, "hello", 200.0, True, [1, 2, 3 ]]
+print(type(nubers2))
+print(numbers[0])
+print(items[1])
+print(items[2])
+trouble = [20,[30,[100,20,[500]]]]
+print(trouble[1][1][2][0])
+#append is to add an a value to a list
+trouble.append(40)
+print(trouble)
+#pop is to remove a value in a list
+trouble.pop()
+print(trouble)
+
+#althemetic
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+#/ allows decimal points
+print(num1/num2)
+#// eliminates decimal points
+print(num1//num2)
+#powers the value
+print(num1**num2)
+#% modulers
+print(num1%num2)
+print(num2%num1)
+
+#assigment operator 
+num1 = 10
+num1 += 10 
+num1 *= 10
+num1 -= 10
+num1 %= 10
+num1 /= 10
+
+#for loops
+num1 = 10
+for i in range(10):
+ print(num1)
+#it will print numbers from 0 to 9
+
